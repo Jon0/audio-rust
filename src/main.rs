7662844250235a0c2f83749d48c5ee8484bcd23f
Jon0 <Jono4728@gmail.com>
2016-11-05 00:18:43 +1300

    sequences

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,47 @@
 use std::io;
 
 
+trait Seq<T> {
+    fn range(&self, start: usize, end: usize) -> &[T];
+}
+
+
+struct PrimeSeq {
+    values: Vec<u64>,
+    max: u64,
+}
+
+
+impl Seq<u64> for PrimeSeq {
+    fn range(&self, start: usize, end: usize) -> &[u64] {
+        return &self.values[start..end];
+    }
+}
+
+
+fn isqrt(number: u64) -> u64 {
+    return (number as f64).sqrt().ceil() as u64;
+}
+
+
+fn find_primes(ps: &mut PrimeSeq, max: u64) {
+    let mut number = ps.max;
+    while number < max {
+        let mut isprime = true;
+        for i in 0..isqrt(number) {
+            if number % i == 0 {
+                isprime = false;
+            }
+        }
+        if isprime {
+            ps.values.push(number);
+        }
+        number += 1;
+    }
+    ps.max = max;
+}
+
+
 fn printc(text: String, colour: u32) {
     println!("\x1b[1;{};1m{}", colour, text);
 }
@@ -19,7 +60,7 @@ fn asknumber() -> u32 {
 }
 
 
-fn sequence(start: u32, size: usize) {
+fn sequence(seq: &Seq<u64>, start: u32, size: usize) {
     let mut grid = vec![0; size * size];
     let start: usize = start as usize;
     for i in 0..size*size {
@@ -36,5 +77,7 @@ fn sequence(start: u32, size: usize) {
 
 
 fn main() {
-    sequence(asknumber(), 8);
+    let mut primes = PrimeSeq { values: vec![], max: 0 };
+    find_primes(&mut primes, 2000);
+    sequence(&primes, asknumber(), 8);
 }
